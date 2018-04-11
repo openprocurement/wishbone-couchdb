f00b45573401fa508d4d5c8e6f21d181429a0b54
Yaroslav Shalenyk <yshalenyk@quintagroup.com>
2018-04-11 18:44:10 +0300

    Added tests

---
+++ tests/test_input.py
@@ -0,0 +1,42 @@
+from gevent import monkey; monkey.patch_all()
+import pytest
+import couchdb
+from wishbone.event import Event
+from wishbone.actor import ActorConfig
+from wishbone.utils.test import getter
+
+from wishbonecouchdb import CouchdbPoller
+
+
+DB_HOST = "admin:admin@127.0.0.1"
+DB_PORT = "5984"
+DB_NAME = "test"
+couchdb_url = 'http://{}:{}'.format(DB_HOST, DB_PORT)
+
+
+
+@pytest.fixture(scope='function')
+def db(request):
+    SERVER = couchdb.Server(couchdb_url)
+    def delete():
+        if DB_NAME in SERVER:
+            del SERVER[DB_NAME]
+
+    if DB_NAME in SERVER:
+        delete()
+    SERVER.create(DB_NAME)
+    request.addfinalizer(delete)
+
+
+def test_couchdb_input(db):
+    config = ActorConfig('couchdbpoller', 100, 1, {}, "")
+    module = CouchdbPoller(
+        config,
+        couchdb_url="{}/{}".format(couchdb_url, DB_NAME),
+    )
+
+    module.pool.queue.outbox.disableFallThrough()
+    module.start()
+    module.couchdb.save({"data": "data"})
+    one = getter(module.pool.queue.outbox)
+    assert one.get().get('data') == "data"
---
+++ tests/test_output.py
@@ -0,0 +1,48 @@
+from gevent import monkey; monkey.patch_all()
+import pytest
+import couchdb
+from wishbone.event import Event
+from wishbone.actor import ActorConfig
+from wishbone.utils.test import getter
+from gevent import sleep
+
+from wishbonecouchdb import CouchdbPuller
+
+
+DB_HOST = "admin:admin@127.0.0.1"
+DB_PORT = "5984"
+DB_NAME = "test"
+couchdb_url = 'http://{}:{}'.format(DB_HOST, DB_PORT)
+
+
+
+@pytest.fixture(scope='function')
+def db(request):
+    SERVER = couchdb.Server(couchdb_url)
+    def delete():
+        if DB_NAME in SERVER:
+            del SERVER[DB_NAME]
+
+    if DB_NAME in SERVER:
+        delete()
+    SERVER.create(DB_NAME)
+    request.addfinalizer(delete)
+
+
+def test_couchdb_output(db):
+    config = ActorConfig('couchdbpoller', 100, 1, {}, "")
+    module = CouchdbPuller(
+        config,
+        couchdb_url="{}/{}".format(couchdb_url, DB_NAME),
+    )
+
+    module.pool.queue.inbox.disableFallThrough()
+    module.start()
+    
+    data = {"data": "data", "_id": "test_doc"}
+    e = Event(data)
+    module.pool.queue.inbox.put(e)
+    sleep(0.1)
+    
+    doc = module.couchdb.get("test_doc")
+    assert doc['data'] == 'data'
