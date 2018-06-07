ef2144180bb0fe0e280978238d337846ee12443f
vladkhard <vladkhard@gmail.com>
2018-06-07 19:11:47 +0300

    add test for filter

---
+++ tests/test_filter.py
@@ -0,0 +1,62 @@
+from gevent import monkey; monkey.patch_all()
+import pytest
+import mock
+import couchdb
+from couchdb.design import ViewDefinition
+from wishbone.event import Event
+from wishbone.actor import ActorConfig
+from gevent import sleep
+
+from wishbonecouchdb import CouchdbFilter
+
+DB_HOST = "admin:admin@127.0.0.1"
+DB_PORT = "5984"
+DB_NAME = "test"
+couchdb_url = 'http://{}:{}'.format(DB_HOST, DB_PORT)
+
+@pytest.fixture(scope='function')
+def db(request):
+    SERVER = couchdb.Server(couchdb_url)
+    def delete():
+        if DB_NAME in SERVER:
+            del SERVER[DB_NAME]
+
+    delete()
+    db = SERVER.create(DB_NAME)
+    view = ViewDefinition('test', 'all', """
+        function (doc) { emit("test", doc._id); }
+    """)
+    view.sync(db)
+    request.addfinalizer(delete)
+
+def test_coucdb_filter(db):
+    config = ActorConfig('couchdbpoller', 100, 1, {}, "")
+    module = CouchdbFilter(
+        actor_config=config,
+        couchdb_url="{}/{}".format(couchdb_url, DB_NAME),
+        view_path="test/all",
+        filter_key='"test"',
+        filter_value='if .mode == "test" then "" else ._id end',
+        op="eq"
+    )
+    data = {"data": "data", "_id": "test_doc", "id": "test_doc"}
+    module.couchdb.save(data)
+    module.logging.debug = mock.MagicMock()
+
+    module.pool.queue.outbox.disableFallThrough()
+    module.pool.queue.inbox.disableFallThrough()
+    module.start()
+
+    e = Event(data)
+    module.pool.queue.inbox.put(e)
+    sleep(1)
+    module.logging.debug.assert_called_with("Event from inbox {}".format(e))
+
+    data = module.couchdb.get("test_doc")
+    data["mode"] = "test"
+    module.couchdb.save(data)
+    e = Event(data)
+    module.pool.queue.inbox.put(e)
+    sleep(1)
+    module.logging.debug.assert_called_with(
+        'Skipped test_doc by filter. Op: eq old: test_doc new ')
